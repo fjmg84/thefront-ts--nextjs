3f91a4e47d9714d164227d54299fc1e84fa899a2
Fidel de J. Miranda Gallego <69002384+fjmg84@users.noreply.github.com>
2024-01-29 15:47:19 -0500

    add getstaticprops

--- src/pages/my-code-test/index.tsx
+++ src/pages/my-code-test/index.tsx
@@ -2,47 +2,34 @@ import React from 'react';
 import LayoutMyCodeTest from 'layouts/MyCodeTest';
 import { Grid } from '@mui/material';
 import MenuWithOutImageComponent from 'components/my-code-test/Menu/Menu';
+import { Menu } from 'types/my-code-test/type';
 
-const MENU_PRINCIPAL = [{
-  name: 'Nosotros',
-  href: '#'
-}, {
-  name: 'Contáctanos',
-  href: '#'
-}, {
-  name: 'Conoce más',
-  href: '#'
-}];
+interface Props {
+  menuDefault: Menu[],
+  menuHomePage: Menu[],
+}
 
-const MENU_HOME = [{
-  name: 'Tramites en linea',
-  href: '#'
-}, {
-  name: 'Ahorro e inversion',
-  href: '#'
-}, {
-  name: 'Empresa',
-  href: '#'
-},
-{
-  name: 'Pension',
-  href: '#'
-}, {
-  name: 'Descuento',
-  href: '#'
-}, {
-  name: 'Eventos',
-  href: '#'
-}];
+export default function MyCodeTestHome(props:Props){
 
-export default function MyCodeTestHome(){
+  const {menuDefault, menuHomePage} = props;
   
-  return <LayoutMyCodeTest menu={MENU_PRINCIPAL} menuPage={MENU_HOME}>
+  return <LayoutMyCodeTest menu={menuDefault} menuPage={menuHomePage}>
     
     <Grid container sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}>
-      <MenuWithOutImageComponent menu={MENU_HOME} />
+      <MenuWithOutImageComponent menu={menuHomePage} />
     </Grid> 
 
 
   </LayoutMyCodeTest>;
+}
+
+import data from '../../mock/data.json';
+
+export async function getStaticProps() {
+  return {
+    props: {
+      menuDefault: data.menu,
+      menuHomePage: data.menuHomePage
+    }
+  };
 }
